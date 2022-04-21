1c9527c28451da7649544990c2a3c1896d4fb1cd
Gianluca Amato <gianluca.amato.74@gmail.com>
2022-04-21 15:16:22 +0200

    Add examples on reaching definitions.

--- src/main/scala/examples/it/unich/scalafixexamples/ScalaFixExamples.scala
+++ src/main/scala/examples/it/unich/scalafixexamples/ScalaFixExamples.scala
@@ -27,6 +27,111 @@ import it.unich.scalafix.finite.KleeneSolver
 import it.unich.scalafix.utils.Relation
 
 import parma_polyhedra_library._
+import it.unich.scalafix.finite.RoundRobinSolver
+import it.unich.scalafix.finite.WorkListSolver
+import it.unich.scalafix.finite.GraphEquationSystem
+import it.unich.scalafix.lattice.Magma
+import it.unich.scalafix.lattice.Domain
+
+object ReachingDefinitionsExample extends App {
+
+  var eqs: FiniteEquationSystem[Int, Set[Int]] = FiniteEquationSystem(
+    body = { rho => {
+        case 1 => Set(1) -- Set(4, 7)
+        case 2 => Set(2) ++ (rho(1) -- Set(5))
+        case 3 => Set(3) ++ (rho(2) -- Set())
+        case 4 => Set(4) ++ (rho(3) ++ rho(7) ++ rho(6) -- Set(1,7))
+        case 5 => Set(5) ++ (rho(4) -- Set(2))
+        case 6 => Set(6) ++ rho(5) -- Set(3)
+        case 7 => Set(6) ++ rho(5) -- Set(1,4)
+      }
+    },
+    inputUnknowns = Set(),
+    initial = Set[Int](),
+    unknowns = Range(1, 8),
+    infl = Relation(Map(1 -> Set(2), 2 -> Set(3), 3 -> Set(4), 4 -> Set(5), 5 -> Set(6,7), 6 -> Set(4), 7 -> Set(4)))
+  )
+  val sol = WorkListSolver(eqs)()
+  println(sol)
+}
+
+
+object ReachingDefinitionsExampleGraph extends App {
+
+  implicit object SetIntDomain extends Domain[Set[Int]] {
+    def upperBound(x: Set[Int], y: Set[Int]) = x ++ y
+    def tryCompare(x: Set[Int], y: Set[Int]): Option[Int] = 
+      if (x subsetOf y)
+        if (x == y)
+          Some(0)
+        else
+          Some(-1)
+      else if (y subsetOf x)
+        Some(1)
+      else None
+    def lteq(x: Set[Int], y: Set[Int]): Boolean = x subsetOf y
+  }
+
+  var eqs: GraphEquationSystem[Int, Set[Int], String] = GraphEquationSystem(
+    unknowns = Range(0,8),
+    inputUnknowns = Set(),
+    edgeAction = { rho =>  {
+        case "01" => Set(1) ++ (rho(0) -- Set(4, 7))
+        case "12" => Set(2) ++ (rho(1) -- Set(5))
+        case "23" => Set(3) ++ (rho(2) -- Set())
+        case "3674" => Set(4) ++ (rho(3) ++ rho(6) ++ rho(7) -- Set(1,7))
+        case "45" => Set(5) ++ (rho(4) -- Set(2))
+        case "56" => Set(6) ++ rho(5) -- Set(3)
+        case "57" => Set(6) ++ rho(5) -- Set(1,4)
+      }
+    },
+    source = {
+      case "01" => Set(0)
+      case "12" => Set(1)
+      case "23" => Set(2)
+      case "3674" => Set(3,6,7)
+      case "45" => Set(4)
+      case "56" => Set(5)
+      case "57" => Set(5)
+    },
+    target = {
+      case "01" => 1
+      case "12" => 2
+      case "23" => 3
+      case "3674" => 4
+      case "45" => 5
+      case "56" => 6
+      case "57" => 7
+    },
+    outgoing = {
+      case 0 => Set("01")
+      case 1 => Set("12")
+      case 2 => Set("23")
+      case 3 => Set("3674")
+      case 4 => Set("45")
+      case 5 => Set("56", "57")
+      case 6 => Set("3674")
+      case 7 => Set("3674")
+    },
+    ingoing = {
+      case 0 => Set()
+      case 1 => Set("01")
+      case 2 => Set("12")
+      case 3 => Set("23")
+      case 4 => Set("3674")
+      case 5 => Set("45")
+      case 6 => Set("56")
+      case 7 => Set("57")
+    },
+    initial = Set[Int](),
+  )
+  val sol = WorkListSolver(eqs)()
+  println(sol)
+}
+
+
+
+
 
 object PolyhedronExample extends App {
   PPLLoader()
@@ -223,7 +328,7 @@ object JPPLBoxExample extends App {
     initial =
       new DoubleBox(
         1,
-        Domain.DegenerateElement.EMPTY
+        it.unich.jppl.Domain.DegenerateElement.EMPTY
       ), // InputAssignment.conditional(3, 10.0, 0.0),
     unknowns = Set(0, 1, 2, 3),
     infl = Relation(Map(0 -> Set(1), 1 -> Set(2), 2 -> Set(3), 3 -> Set(1)))
